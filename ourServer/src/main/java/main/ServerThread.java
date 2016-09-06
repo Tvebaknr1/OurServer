95b6c1a16c6121fb66b0706473fc42924a5afd42
EmilUlrikChristoffersen <emilulrikchristoffersen@outlook.dk>
2016-09-06 12:54:45 +0200

    non commit

--- ourServer/src/main/java/main/ServerThread.java
+++ ourServer/src/main/java/main/ServerThread.java
@@ -1,8 +1,4 @@
-﻿/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package main;
 
 import java.io.IOException;
@@ -24,7 +20,7 @@ public class ServerThread extends Thread implements ObserverInterface{
 
     Scanner scr;
     PrintWriter prnt;
-    public SeverThread(Socket s) {
+    public ServerThread(Socket s) {
 
         this.s = s;
         try{
