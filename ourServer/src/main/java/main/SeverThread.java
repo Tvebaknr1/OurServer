bec96fd7ab91c36b0193735f7af1aa6805d95f92
EmilUlrikChristoffersen <emilulrikchristoffersen@outlook.dk>
2016-09-06 12:07:29 +0200

    to the hive mind!

---
+++ ourServer/src/main/java/main/SeverThread.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Observable;
+import java.util.Observer;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Emil
+ */
+public class SeverThread extends Thread implements ObserverInterface{
+
+    private Socket s;
+
+    public SeverThread(Socket s) {
+        this.s = s;
+    }
+
+    private void handleclient(Socket s) {
+        try {
+            Scanner scr = new Scanner(s.getInputStream());
+            PrintWriter prnt = new PrintWriter(s.getOutputStream(), true);
+            String msg = "";
+            prnt.println("wellcome");
+            boolean loggedin = false;
+            
+            while (!loggedin) {
+                if(msg != ""){
+                    
+                }
+            }
+            while (loggedin) {
+
+            }
+            scr.close();
+            prnt.close();
+            s.close();
+        } catch (IOException ex) {
+            System.out.println(ex);
+        }
+    }
+
+    @Override
+    public void run() {
+        handleclient(s);
+    }
+
+    @Override
+    public void update() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+   
+
+}
--- ourServer/src/main/java/main/socketmain.java
+++ ourServer/src/main/java/main/socketmain.java
@@ -6,81 +6,59 @@
 package main;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
  * @author Emil
  */
-public class socketmain {
-
+public class socketmain implements StaticSubjectInterface{
+    static ArrayList<ObserverInterface> observers = new ArrayList<>();
     static String ip = "localhost";
     static int portNum = 8080;
-    
-    
+    private static ArrayList<Thread> thread = new ArrayList<>();
+
     public static void main(String[] args) throws IOException, InterruptedException {
         if (args.length == 2) {
             ip = args[0];
             portNum = Integer.parseInt(args[1]);
 
         }
-        ServerSocket ss = new ServerSocket();
-        ss.bind(new InetSocketAddress(ip, portNum));
         ArrayList socketArray = new ArrayList();
-        socketArray.add(ss);
-        while (true) {
+        ServerSocket ss = new ServerSocket();
+            ss.bind(new InetSocketAddress(ip, portNum));
+            
 
-            Socket link = ss.accept();
+
+           
+        while (true) {
+             Socket link = ss.accept();
+            socketArray.add(link);
 
             System.out.println("new client connection");
-            handleclient(link);
+            SeverThread t = new SeverThread(link);
+            t.start();
+            
+            thread.add(t);
+           
         }
 
     }
-    private static void handleclient(Socket s){
-        try {
-            Scanner scr = new Scanner(s.getInputStream());
-            PrintWriter prnt = new PrintWriter(s.getOutputStream(),true);
-            String msg = "";
-            prnt.println("wellcome");
-            
-            while (!msg.equals("STOP")) {
-                msg = scr.nextLine();
-                if(msg.length()>=6&&msg.substring(0, 6).equals("UPPER#"))
-                {
-                    prnt.println(msg.substring(6, msg.length()).toUpperCase());
-                }
-                else if(msg.length()>=6&&msg.substring(0, 6).equals("LOWER#")){
-                    prnt.println(msg.substring(6,msg.length()).toLowerCase());
-                }
-                else if (msg.length()>=8&&msg.substring(0, 8).equals("REVERSE#")){
-                    String str = new StringBuilder(msg.substring(8, msg.length())).reverse().toString();
-                    prnt.println(str.substring(0,1).toUpperCase()+str.substring(1, str.length()));
-                }
-                else if(msg.length()>=10&&msg.substring(0, 10).equals("TRANSLATE#")){
-                    switch(msg.substring(10, msg.length()).toLowerCase()){
-                        case"hund":{
-                            prnt.println("dog");
-                        }
-                        default:{
-                            prnt.println("#NOT_FOUND");
-                        }
-                    }
-                }
-                
-            }
-            scr.close();
-            prnt.close();
-            s.close();
-        } catch (IOException ex) {
-            System.out.println(ex);
+
+   // @Override
+    public static void register(ObserverInterface o) {
+        observers.add(o);
+    }
+
+   // @Override
+    public static void notifyObserver() {
+        for(ObserverInterface o: observers)
+        {
+            o.update();
         }
     }
+
 }
